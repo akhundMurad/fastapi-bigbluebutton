ba2c1bd902d56a1000cf90f95e684f89693d999c
akhundMurad <akhundov1murad@gmail.com>
2021-07-08 20:43:41 +0400

    Meeting Schedule ScheduleCell models added

---
+++ src/core/models/bigbluebutton.py
@@ -0,0 +1,57 @@
+import ormar
+
+from datetime import datetime
+from typing import List, Optional
+
+from core.db import metadata, database
+from core.models.users import User
+
+
+class Schedule(ormar.Model):
+    class Meta:
+        tablename = 'schedule'
+        metadata = metadata
+        database = database
+
+    id: int = ormar.Integer(primary_key=True, autoincrement=True)
+    attendee_list: Optional[List[User]] = ormar.ManyToMany(User)
+
+
+class ScheduleCell(ormar.Model):
+    class Meta:
+        tablename = 'schedule_cell'
+        metadata = metadata
+        database = database
+
+    id: int = ormar.Integer(primary_key=True, autoincrement=True)
+    datetime_start: datetime = ormar.DateTime()
+    datetime_end: datetime = ormar.DateTime()
+
+    schedule: Schedule = ormar.ForeignKey(Schedule, related_name='cells')
+
+
+class Meeting(ormar.Model):
+    class Meta:
+        tablename = 'meeting'
+        metadata = metadata
+        database = database
+
+    id: str = ormar.UUID(primary_key=True, uuid_format='string')
+    name: str = ormar.String(max_length=512)
+
+    welcome_message: Optional[str] = ormar.String(max_length=128)
+    moderator_message: Optional[str] = ormar.String(max_length=128)
+    record: bool = ormar.Boolean(default=False)
+    auto_start_recording: bool = ormar.Boolean(default=False)
+    allow_start_stop_recording: bool = ormar.Boolean(default=True)
+    webcams_only_for_moderator: bool = ormar.Boolean(default=False)
+    mute_on_start: bool = ormar.Boolean(default=False)
+    allow_mods_to_unmute_users: bool = ormar.Boolean(default=False)
+    max_participants: Optional[int] = ormar.Integer(maximum=50, nullable=True)
+    duration: Optional[int] = ormar.Integer(nullable=True)
+
+    schedule_cell: ScheduleCell = ormar.ForeignKey(ScheduleCell,
+                                                   related_name='meetings')
+
+    datetime_start: datetime = ormar.DateTime()
+    datetime_end: datetime = ormar.DateTime()
